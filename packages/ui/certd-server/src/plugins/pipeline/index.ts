25cb15cd46ff843bb904f5319a8c6d95e963913c
盛俊鹏 <shengjunpeng@yiwise.com>
2023-10-13 18:51:41 +0800

    feat: 测试

--- packages/ui/certd-server/src/plugins/pipeline/index.ts
+++ packages/ui/certd-server/src/plugins/pipeline/index.ts
@@ -1,6 +1,5 @@
-// src/index.ts
-import '@certd/plugin-all';
-// export { PipelineConfiguration as Configuration } from '@certd/pipeline';
-// export * from './controller/user';
-// export * from './controller/api';
-// export * from './service/user';
+import '../plugin-aliyun';
+import '../plugin-cert';
+import '@certd/plugin-host';
+import '../plugin-huawei';
+import '../plugin-tencent';
